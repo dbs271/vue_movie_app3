5256853be9389483452c798b2eaa9faa6b18680e
dbs271 <djaudwls7873@gmail.com>
2022-10-19 00:11:04 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'GUK',
+    email: 'djaudwls7873@gamil.com',
+    github: 'https://github.com/dbs271',
+    phone: '+82-10-3333-0000',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
