5be2ef3a070bfa8c1d9a8186e81b8d95c11f9a24
dbs271 <djaudwls7873@gmail.com>
2022-10-19 23:16:01 +0900

    Add Netlify functions

---
+++ functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: 'dbs',
+      age: 24,
+      email: 'dbs2747@gamil.com'
+    })
+  }
+}
